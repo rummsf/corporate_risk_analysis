0194909a8d6b15ce599e4c5c159fb9f6914b0dac
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-10-25 14:05:21 +0100

    authentication to companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,15 +1,15 @@
 class CompaniesController < ApplicationController
   def index
-  authorized?
-  respond_to do |format|
-    if params[:term]
-      @companies = Company.search_by_name(params[:term]).with_pg_search_highlight
-    else
-      @companies = Company.all
+    authorized?
+    respond_to do |format|
+      if params[:term]
+        @companies = Company.search_by_name(params[:term]).with_pg_search_highlight
+      else
+        @companies = Company.all
+      end
+      format.json
+      format.html
     end
-    format.json
-    format.html
-  end
   end
 
   def show
